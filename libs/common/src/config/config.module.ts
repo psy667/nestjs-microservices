1e23455f4ff23605c0e18afa16912518c48db8d6
Arseniy Oguzov <oguzov.a@n-t.io>
2023-07-20 22:38:38 +0400

    feat: basic structure and abstract repository

---
+++ libs/common/src/config/config.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import {
+  ConfigModule as NestConfigModule,
+  ConfigService,
+} from '@nestjs/config';
+import * as Joi from 'joi';
+
+@Module({
+  exports: [ConfigService],
+  imports: [
+    NestConfigModule.forRoot({
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+      }),
+    }),
+  ],
+  providers: [ConfigService],
+})
+export class ConfigModule {}
---
+++ libs/common/src/database/database.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigService } from '@nestjs/config';
+import { ConfigurableModuleClass } from './database.module-definition';
+import { ConfigModule } from '@app/common/config';
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+})
+export class DatabaseModule extends ConfigurableModuleClass {}
